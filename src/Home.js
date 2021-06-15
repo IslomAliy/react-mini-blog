b71d8431d6e07b0a81cf61e4ba732c866a2ecbe7
islomaliy <kislombek71@gmail.com>
2021-06-15 22:47:38 +0500

    add useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,10 +8,15 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ]);
 
-      const handleDelete = (id) => {
+    const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
       }
+
+    useEffect(()=>{
+      console.log('useEffect ran');
+    })
+
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs:" handleDelete={handleDelete}/>
